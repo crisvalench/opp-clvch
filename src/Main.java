f867a777f61e3dd19f3b5773eacc67964d8b221a
crisvalench <cristinavalenzuela2002@hotmail.com>
2024-05-31 10:51:54 -0500

    Add(I): Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Example for printing method
+    printLine(6);
+    printLine(5);
+        }
+    public static void printLine(int num){
+        for(int i = 0; i < num; i++){
+            System.out.print("* ");
+        };
+        System.out.println();
+    }
+    }
+
